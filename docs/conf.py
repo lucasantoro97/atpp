ec9fb25cb9c3c04b5f36d373214d88e9c329adf3
LucaSantoro97 <lcsantoro97@gmail.com>
2024-09-16 14:33:45 +0200

    Refactor documentation build process: Add autosummary extension and configure autodoc options

--- docs/conf.py
+++ docs/conf.py
@@ -21,7 +21,17 @@ release = '0.1'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.napoleon', 'sphinx.ext.viewcode']
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.napoleon', 'sphinx.ext.viewcode', 'sphinx.ext.autosummary']
+autodoc_default_options = {
+    'members': True,         # Include all class and module members
+    'undoc-members': True,   # Include members without docstrings
+    'private-members': False, # Include private members (e.g., _foo) if set to True
+    'special-members': '__init__', # Include special methods like __init__
+    'inherited-members': True,  # Include members inherited from parent classes
+    'show-inheritance': True   # Show class inheritance diagram
+}
+autosummary_generate = True
+
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store','_modules']
