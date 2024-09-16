5c8577ad710b9c850af3891c7caf880213f261c6
LucaSantoro97 <lcsantoro97@gmail.com>
2024-09-16 19:41:04 +0200

    Add console script entry point to setup.py

--- setup.py
+++ setup.py
@@ -32,4 +32,9 @@ setup(
         'Operating System :: OS Independent',
     ],
     python_requires='>=3.6',
+    entry_points={
+        'console_scripts': [
+            'atpp=cli:main',
+        ],
+    },
 )
