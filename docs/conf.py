c8586bda1e53c493d153c716910d391d85bea203
LucaSantoro97 <lcsantoro97@gmail.com>
2024-09-16 14:14:31 +0200

    Exclude _modules directory from documentation build

--- docs/conf.py
+++ docs/conf.py
@@ -24,7 +24,7 @@ release = '0.1'
 extensions = ['sphinx.ext.autodoc', 'sphinx.ext.napoleon', 'sphinx.ext.viewcode', 'sphinx.ext.autosummary']
 
 templates_path = ['_templates']
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store','_modules']
 
 
 
