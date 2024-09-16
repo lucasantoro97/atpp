7fc62bc4334c5ec426cc0def989e3b1763729bee
LucaSantoro97 <lcsantoro97@gmail.com>
2024-09-16 14:26:12 +0200

    Refactor documentation build process

--- docs/conf.py
+++ docs/conf.py
@@ -21,7 +21,7 @@ release = '0.1'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.napoleon', 'sphinx.ext.viewcode', 'sphinx.ext.autosummary']
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.napoleon', 'sphinx.ext.viewcode']
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store','_modules']
