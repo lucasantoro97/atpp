481474e2cd1fa7529ef3232f2585353e30f19dc9
LucaSantoro97 <lcsantoro97@gmail.com>
2024-10-14 14:07:50 +0200

    Refactor logging configuration

--- atpp/logging_config.py
+++ atpp/logging_config.py
@@ -1,3 +1,20 @@
+    """_summary_
+    This module configures the logging for the application.
+
+    It sets up logging to output messages to both the terminal and a log file.
+    The logging level is set to INFO by default, but can be changed to DEBUG for more detailed logs.
+
+    Logging Configuration:
+    - Level: INFO (default), can be changed to DEBUG
+    - Format: '%(asctime)s - %(levelname)s - %(filename)s:%(lineno)d - %(message)s'
+    - Handlers:
+        - StreamHandler: Outputs logs to the terminal (stdout)
+        - FileHandler: Outputs logs to 'ThernessLibCore.log' file
+    - Force: True (forces reconfiguration of logging)
+
+    Logger:
+    - Name: 'therness'
+    """
 import logging
 import sys
 
